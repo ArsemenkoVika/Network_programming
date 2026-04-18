42c0e64a61bfefe0bf6ac6af484714436a3eeae4
Arsemenko Viktoria <arsemenko.vika@mail.ru>
2026-04-18 14:53:18 +0300

    fixed empty strings, exit and stop_server commands

--- server.py
+++ server.py
@@ -1,14 +1,47 @@
 import socket
 
-def start_server():
+def run_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('localhost',12345))
     server.listen(1)
-    server.settimeout(1.0)
-
     print('Сервер запущен и ждет клиента...')
-    return server
 
-if _name_ == '_main':
-    s = start_server()
-    s.close()
+    server_running = True
+    while server_running:
+        try:
+            client,address = server.accept()
+            print(f'Подключился клиент: {address}')
+
+            while True:
+                try:
+                    data = client.recv(1024).decode('utf-8')
+                    if not data:
+                        continue
+
+                    print(f'клиент прислал: {data} ')
+
+                    if data == 'exit':
+                        print('клиент решил отключиться')
+                        break
+
+                    if data == 'stop_server':
+                        print('получена команда выключения сервера')
+                        server_running = False
+                        break
+
+                    client.send('сообщение получено'.encode('utf-8'))
+
+                except ConnectionResetError:
+                    print('связь с клиентом разорвана')
+                    break
+
+            client.close()
+        except Exception:
+            break
+
+    server.close()
+    print("Сервер полностью остановлен.")
+
+
+if __name__ == "__main__":
+    run_server()
